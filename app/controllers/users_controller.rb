9c812daafeee4d7c7b27caa0bf925a1eabac79d1
Jeff Wolak <jeffrey_wolak@yahoo.com>
2013-03-08 08:43:22 -0500

    added filter for signed in users to keep from creating a new account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
   before_filter :correct_user,   only: [:edit, :update]
   before_filter :admin_user,     only: :destroy
+  before_filter :not_signed_in,    only: [:new, :create]
 
   def show
     @user = User.find(params[:id])
@@ -61,5 +62,11 @@ class UsersController < ApplicationController
 
     def admin_user
       redirect_to(root_path) unless current_user.admin?
+    end
+
+    def not_signed_in
+      if signed_in?
+        redirect_to root_path, notice: "You are already signed in."
+      end
     end
 end
